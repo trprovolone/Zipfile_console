17432baa8c75d3c13e0435ca05a68cb4a222c056
trprovolone <twhite1201ftw@gmail.com>
2016-07-29 18:47:25 -0400

    added directory functionality

--- zipfile/Program.cs
+++ zipfile/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,38 @@ namespace zipfile
     {
         static void Main(string[] args)
         {
+
+            var path = args[0];
+
+            var dirInfo = new DirectoryInfo(path);
+
+            if (!dirInfo.Exists)
+            {
+                Console.WriteLine("Invalid path.");
+                return;
+            }
+            
+            var name = dirInfo.Name;
+            var fullName = dirInfo.FullName;
+
+            var files = dirInfo.GetFiles().ToList();
+            var folders = dirInfo.GetDirectories().ToList();
+
+            foreach (var file in files)
+            {
+                var fileName = file.Name;
+
+                Console.WriteLine(fileName);
+
+
+            }
+
+            foreach (var folder in folders)
+            {
+                var folderName = folder.Name;
+                Console.WriteLine(folderName);
+            }
+
         }
     }
 }
